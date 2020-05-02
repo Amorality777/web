eba41c19b62096806e64ca79424d5c4aea4bc9a6
Egor Zh <amorality777@mail.ru>
2020-05-02 18:57:12 +0300

    task_2_v1

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo ln -sf ~/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf ~/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/nginx restart
